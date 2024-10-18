4b015462f287cc46a8115e83ef4314c6ef49101b
Domenico Germanà <Mirkogermana12@gmail.com>
2024-10-18 17:42:21 +0200

    feat: added User class to manage users

---
+++ src/main/java/com/library/management/users/User.java
@@ -0,0 +1,49 @@
+package com.library.management.users;
+
+public class User {
+    private int userID;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public User(int userID, String firstName, String lastName, String username, String password) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
