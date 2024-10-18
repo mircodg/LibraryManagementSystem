c997dadd6ff6eeb55c5c1bf559250b72e6722049
Domenico Germanà <Mirkogermana12@gmail.com>
2024-10-18 17:41:33 +0200

    feat: added Book class to manage books

--- src/main/java/com/library/management/books/Book.java
+++ src/main/java/com/library/management/books/Book.java
@@ -1,4 +1,59 @@
 package com.library.management.books;
 
+import java.util.Date;
+
 public class Book {
+
+    private int bookID;
+    private String title;
+    private String author;
+    private String description;
+    private String publisher;
+    private Date publishedDate;
+
+    public Book(int bookID, String title, String author, String description, String publisher, Date publishedDate) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
 }
