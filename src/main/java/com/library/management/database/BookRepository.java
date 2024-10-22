2973352bdc271d5f3aa753b71457ea95fcfa9ada
mircodev <Mirkogermana12@gmail.com>
2024-10-22 23:34:07 +0200

    feat: improved logic and added methods to retrieve books

--- src/main/java/com/library/management/database/BookRepository.java
+++ src/main/java/com/library/management/database/BookRepository.java
@@ -1,4 +1,5 @@
 package com.library.management.database;
+
 import com.library.management.books.Book;
 
 import java.sql.Connection;
@@ -7,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Date;
+import java.sql.Date;
 
 
 public class BookRepository {
@@ -19,72 +20,111 @@ public class BookRepository {
     }
 
     public Book getBookByID(int bookID) {
-        try{
-            String query = "SELECT * FROM books WHERE bookID = ?" ;
+        try {
+            String query = "SELECT * FROM books WHERE bookID = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, bookID);
             ResultSet rs = statement.executeQuery();
-            return new Book(rs.getInt("bookID"), rs.getString("title"), rs.getString("author"), rs.getString("description"), rs.getString("publisher"), rs.getDate("publishedDate"));
-        }catch(SQLException e){
+            if (rs.next()) {
+                Date date = rs.getDate("publishedDate");
+                String dateAsString = date.toString();
+                return new Book(rs.getInt("bookID"), rs.getString("title"), rs.getString("author"), rs.getString("description"), rs.getString("publisher"), dateAsString);
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
             System.err.println("error while retrieving book: " + e.getMessage());
             return null;
         }
     }
 
     public boolean addBook(Book book) {
-        try{
-            String query = "INSERT INTO books (bookID, title, description, author, publisher, publishedDate) VALUES (?, ?, ?, ?, ?, ?))";
+        try {
+            String query = "INSERT INTO books (title, description, author, publisher, publishedDate) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(query);
-            return statement.execute();
-        }catch(SQLException e){
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getDescription());
+            statement.setString(3, book.getAuthor());
+            statement.setString(4, book.getPublisher());
+            statement.setDate(5, book.stringToDate(book.getPublishedDate())); // handle date correctly, see chatgpt
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
             System.err.println("error while adding a book: " + e.getMessage());
             return false;
         }
     }
 
     public List<Book> getAllBooks() {
-        try{
+        try {
             String query = "SELECT * FROM books";
             PreparedStatement statement = connection.prepareStatement(query);
             ResultSet rs = statement.executeQuery();
             List<Book> books = new ArrayList<>();
-            while(rs.next()){
-                books.add(new Book(rs.getInt("bookID"), rs.getString("title"), rs.getString("description"), rs.getString("author"), rs.getString("publisher"), rs.getDate("publishedDate") ));
+            while (rs.next()) {
+                Date date = rs.getDate("publishedDate");
+                String dateAsString = date.toString();
+                books.add(new Book(rs.getInt("bookID"), rs.getString("title"), rs.getString("description"), rs.getString("author"), rs.getString("publisher"), dateAsString));
             }
             return books;
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.err.println("error while retrieving all books: " + e.getMessage());
             return null;
         }
     }
 
-    public boolean updateBook(int bookID, String title, String author, String description, String publisher, Date date) {
-        Book newBook = getBookByID(bookID);
-        if(newBook == null)
-            return false;
-        if(title != null)
-            newBook.setTitle(title);
-        if(author != null)
-            newBook.setAuthor(author);
-        if(description != null)
-            newBook.setDescription(description);
-        if(publisher != null)
-            newBook.setPublisher(publisher);
-        if(date != null)
-            newBook.setPublishedDate(date);
-        return addBook(newBook);
-    }
+//    public boolean updateBook(int bookID, String title, String author, String description, String publisher, String date) {
+//        Book book = getBookByID(bookID);
+//        if (book != null) {
+//
+//        }
+//    }
 
 
     public boolean deleteBook(int bookID) {
-        try{
+        try {
             String query = "DELETE FROM books WHERE bookID = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setInt(1, bookID);
-            return statement.execute();
-        }catch(SQLException e){
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
             System.err.println("error while deleting book: " + e.getMessage());
             return false;
+        }
+    }
+
+    public Book getBookByTitle(String title) {
+        try {
+            String query = "SELECT * FROM books WHERE title = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, title);
+            ResultSet rs = statement.executeQuery();
+            return returnBookFromQuery(rs);
+        } catch (SQLException e) {
+            System.err.println("error while searching a book: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public Book getBookById(int bookID) {
+        try{
+            String query = "SELECT * FROM books WHERE bookID = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, bookID);
+            ResultSet rs = statement.executeQuery();
+            return returnBookFromQuery(rs);
+        }catch(SQLException e){
+            System.err.println("error while searching a book: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private Book returnBookFromQuery(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            Date date = rs.getDate("publishedDate");
+            String dateAsString = date.toString();
+            return new Book(rs.getInt("bookID"), rs.getString("title"), rs.getString("description"), rs.getString("author"), rs.getString("publisher"), dateAsString);
+        }else{
+            return null;
         }
     }
 
