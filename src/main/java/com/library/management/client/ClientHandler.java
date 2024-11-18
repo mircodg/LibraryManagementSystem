ef3bc89be18dbeb37027e366ee0edf834b47d7d7
Domenico Germanà <Mirkogermana12@gmail.com>
2024-11-18 15:55:41 +0100

    feat: added new ClientMenu

--- src/main/java/com/library/management/client/ClientHandler.java
+++ src/main/java/com/library/management/client/ClientHandler.java
@@ -1,5 +1,5 @@
 package com.library.management.client;
-import com.library.management.menu.ClientMenuV2;
+import com.library.management.menu.ClientMenu;
 
 import java.io.*;
 import java.net.Socket;
@@ -61,7 +61,7 @@ public class ClientHandler extends Thread {
     public void run() {
         System.out.println("[SERVER] client thread started");
         try {
-            ClientMenuV2 clientMenu = new ClientMenuV2(this.connection, this.socket);
+            ClientMenu clientMenu = new ClientMenu(this.connection, this.socket);
             clientMenu.displayMenu();
         } catch (IOException | NullPointerException e) {
             this.closeAll();
